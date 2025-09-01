8a901bd1bc2ba7b3e10dce0fd7a027004f7af669
r1no933 <dbaskakov93@yandex.ru>
2025-09-01 18:48:05 +0300

    реализован класс Main согласно ТЗ + fix DataProcessor класс

--- src/main/java/ru/baskakov/Main.java
+++ src/main/java/ru/baskakov/Main.java
@@ -1,17 +1,65 @@
 package ru.baskakov;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final int TOTAL_TASKS = 100;
+
+    public static void main(String[] args) throws InterruptedException {
+        DataProcessor dataProcessor = new DataProcessor(THREAD_POOL_SIZE);
+
+        System.out.println("Starting 100 tasks...");
+        List<String> tasks = generateAndSubmitTasks(dataProcessor);
+        System.out.println("All tasks submitted. Start to complete tasks.");
+
+        while (dataProcessor.getCountOfActiveTasks() > 0) {
+            int activeTasks = dataProcessor.getCountOfActiveTasks();
+            System.out.println("Active tasks: " + activeTasks);
+            Thread.sleep(100);
         }
+
+        System.out.println("All tasks completed!");
+        System.out.println("Final active tasks: " + dataProcessor.getCountOfActiveTasks());
+
+        printTasksResult(tasks, dataProcessor);
+
+        dataProcessor.shutdown();
+
+    }
+
+    public static List<String> generateAndSubmitTasks(DataProcessor dataProcessor) {
+        List<String> tasks = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < TOTAL_TASKS; i++) {
+            List<Integer> numbers = new ArrayList<>();
+            int numsCount = random.nextInt(10) + 1;
+            for (int j = 0; j < numsCount; j++) {
+                numbers.add(random.nextInt(100));
+            }
+
+            String taskName = dataProcessor.submitTask(numbers);
+            tasks.add(taskName);
+        }
+
+        return tasks;
+    }
+
+    public static void printTasksResult(List<String> tasks, DataProcessor dataProcessor) {
+        int sum = 0;
+        for (String task : tasks) {
+            Optional<Integer> result = dataProcessor.getTaskResult(task);
+            result.ifPresent(value -> {
+                System.out.println(task + ": " + value);
+            });
+            sum += result.orElse(0);
+        }
+
+        System.out.println("Sum of all tasks: " + sum);
+        System.out.println("Total tasks executed: " + tasks.size());
     }
 }
